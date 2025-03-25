072b33893739a685e15f924702693e93826f8e12
Sivakumar U <itsmesivakumar21@gmail.com>
2025-03-25 15:41:05 -0400

    [Siva]Added: using Rest Operator to find sum of values, max num, printing rest parameter.

---
+++ Rest_Operator/find_max_number.js
@@ -0,0 +1,5 @@
+function findMax(...nums) {
+  return Math.max(...nums);
+}
+var res = findMax(5, 3, 9, 7, 6, 8);
+console.log(res);
---
+++ Rest_Operator/print_rest_parameter.js
@@ -0,0 +1,4 @@
+function print(a, b, ...rest) {
+  console.log(a, b, rest);
+}
+print(10, 20, 30, 40, 50);
